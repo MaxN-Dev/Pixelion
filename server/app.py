cf8a11fa335cca985482877b90eb9c970c31222a
MaxPepa <maxnelken@gmail.com>
2025-06-19 04:42:25 +0300

    Describe what you changed

--- server/app.py
+++ server/app.py
@@ -52,7 +52,8 @@ threading.Thread(target=periodic_save, daemon=True).start()
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+
 
 
 
